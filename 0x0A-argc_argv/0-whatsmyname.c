49c663bca0248d3f7eb69b0197f9292114e98871
Chidindu24 <chiny2cute@gmail.com>
2023-05-12 17:30:09 -0700

    Argument in C

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
  * @argv: array of arguments
  * Return: 0
  */
-int main(int argc__attribute__((unused)), char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	printf("%s\n", "argv);
+	printf("%s\n", argv[0]);
 	return (0);
 }
