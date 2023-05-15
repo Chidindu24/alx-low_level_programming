fa1661ecde297b4eb9955c7c93a5a713a85eff4e
Chidindu24 <chiny2cute@gmail.com>
2023-05-15 14:03:30 -0700

    Function that concatenates all arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 /**
  * argstostr - concatenate all arguments of your program with newline
  * @ac: argument count
@@ -9,35 +8,31 @@
  */
 char *argstostr(int ac, char **av)
 {
-	char *a, *retp;
-	int i, j, total;
+	int i, n, r = 0, l = 0;
+	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (i = 0, total = 0; i < ac; i++)
-	{
-		for (j = 0
-		; *(*(av + 1) + j)
-		!= '\0'; j++, total++)
-			;
-		total++;
-	}
-	total++;
 
-	a = malloc(total * sizeof(char));
-	if (a == NULL)
-		return (NULL);
-
-	retp = a;
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			*a = av[i][j];
-			a++;
-		}
-		*a = '\n';
-		a++;
+		for (n = 0; av[i][n]; n++)
+			l++;
 	}
-	return (retp);
+	l += ac;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		str[r] = av[i][n];
+		r++;
+	}
+
+	if (str[r] == '\0')
+	{
+		str[r++] = '\n';
+	}
+	return (str);
 }
