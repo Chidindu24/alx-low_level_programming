3453f4f18109c3857a3358877e19d4779fe1ba69
Chidindu24 <chiny2cute@gmail.com>
2023-05-24 14:01:37 -0700

    Opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,28 +8,29 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, nbytes;
-	char *opc = (char *) main;
+	int bytes, i;
+	char *arr;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	nbytes = atoi(argv[1]);
-	if (nbytes < 0)
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	for (i = 0; i < nbytes; i++)
+	arr = (char *)main;
+	for (i = 0; i < bytes; i++)
 	{
-		printf("%02x", opc[i] & 0xFF);
-		if (i != nbytes - 1)
+		if (i == bytes - 1)
 		{
-			printf(" ");
+			printf("%02hhx\n", arr[i]);
+			break;
 		}
-		printf("\n");
+		printf("%02hhx", arr[i]);
 	}
 	return (0);
 }
