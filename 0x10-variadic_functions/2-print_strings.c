d762ccdfdd10e3e1f78d6202e26aea97e8587c83
Chidindu24 <chiny2cute@gmail.com>
2023-05-25 12:09:02 -0700

    Print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,7 +9,7 @@
  * Description: If separator is NULL, it is not printed.
  * if one of the strings if NULL, (nil) is printed instead
  */
-void print_strins(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list strings;
 	char *str;
